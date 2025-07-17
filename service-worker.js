0c1ad60b808bb6d3be2064723636e6239d5b372e
cl4940412 <>
2025-07-17 16:35:31 +0800

    register service worker script

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,21 @@
-// 空白 service worker
+const CACHE_NAME = 'pwa-cache-v1';
+const OFFLINE_URLS = [
+  '/',
+  '/index.html',
+  '/app.js',
+  '/router.js',
+  '/pages/home.html',
+  '/pages/inbound.html'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(OFFLINE_URLS))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
